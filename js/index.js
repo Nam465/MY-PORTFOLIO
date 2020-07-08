23749164c34efc92d57da41ce38d21fb4a4093c8
nam <namggg462@gmail.com>
2020-07-08 19:45:54 +0700

    ...

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 styleWhenScrollTo('header', 10);
 
-// each 3s add a new quote to #quote element 
+// each 10s add a new quote to #quote element 
 setInterval(function renderQuote() {
     let {text, from} = getRandomQuote();
     document.querySelector('.inspire .inspire__text').innerText = text;
     document.querySelector('.inspire .inspire__from').innerText = from;
-}, 7000);
+}, 10000);
