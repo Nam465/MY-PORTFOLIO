25c78300867bf6051b4c2109cf6bee2e54dd98ba
nam <namggg462@gmail.com>
2020-07-16 10:06:29 +0700

    fetch data and render it to UI for all section, except project section

--- js/fetchData.js
+++ js/fetchData.js
@@ -1,8 +1,54 @@
 let inforUrl = 'https://raw.githubusercontent.com/xnameFV/MY-PORTFOLIO/master/data/information.json';
+let aboutUrl = 'https://raw.githubusercontent.com/xnameFV/MY-PORTFOLIO/master/data/aboutMe.json';
+let whatIDoUrl = 'https://raw.githubusercontent.com/xnameFV/MY-PORTFOLIO/master/data/whatIDo.json';
+let skillUrl = 'https://raw.githubusercontent.com/xnameFV/MY-PORTFOLIO/master/data/skills.json';
+let expUrl = 'https://raw.githubusercontent.com/xnameFV/MY-PORTFOLIO/master/data/experiences.json';
+let projectsUrl = 'https://raw.githubusercontent.com/xnameFV/MY-PORTFOLIO/master/data/projects.json';
 
 fetch(inforUrl)
     .then(res => res.json())
     .then(data => {
         renderHeroSection(data);
+    })
+    .catch(err => console.log(err));
+
+
+
+fetch(aboutUrl) 
+    .then(res => res.json())
+    .then(data => {
+        renderAboutSection(data);
+    })
+    .catch(err => console.log(err));
+
+
+fetch(whatIDoUrl) 
+    .then(res => res.json())
+    .then(data => {
+        renderWhatToDoSection(data);
+    })
+    .catch(err => console.log(err));
+
+
+fetch(skillUrl)
+    .then(res => res.json())
+    .then(data => {
+        renderSkillSection(data);
+    })
+    .catch(err => console.log(err));
+
+
+fetch(expUrl)
+    .then(res => res.json())
+    .then(data => {
+        renderExperienceSection(data);
+    })
+    .catch(err => console.log(err));
+
+
+fetch(projectsUrl) 
+    .then(res => res.json())
+    .then(data => {
+        renderProjectSection(data);
     })
     .catch(err => console.log(err));
--- js/renderFunctions.js
+++ js/renderFunctions.js
@@ -8,7 +8,7 @@ function renderHeroSection(infor) {
     document.querySelector('.hero-panel__phone').innerText = phone;
     document.querySelector('.hero-panel__address').innerText = address;
     document.querySelector('.hero-panel__job').innerText = job;
-    document.querySelector('.avata__bg').style.backgroundImage = avataLink;
+    document.querySelector('.avata__bg').style.backgroundImage = `url(${avataLink})`;
     socialIcons.forEach(s => s.style.href=socialLinks);
 }
 
@@ -16,28 +16,98 @@ function renderHeroSection(infor) {
 
 // aboute render
 function renderAboutSection(data) {
-    
+    let {about, tags} = data;
+    let listTags = '';
+    tags.forEach(tag => {
+        listTags += `<span class="border text-1 mr-10">${tag}</span>`;
+    });
+
+    document.querySelector('.about__text').innerText = about;
+    document.querySelector('.about__tags-container').innerHTML = listTags;
 }
 
 
 
 // what to do render
 function renderWhatToDoSection(data) {
-
+    let cards = document.querySelectorAll('.what-i-do .card');
+    for (let i = 0; i < cards.length; i++) {
+        let container = cards[i],
+            h4 = cards[i].querySelector('h4'),
+            cardBody = cards[i].querySelector('.what-i-do .card__body');
+        h4.innerText = data[i].title;
+        cardBody.innerText = data[i].text;
+    }
 }
 
 
 
 // skill render
 function renderSkillSection(data) {
-
+    let {technical, professional} = data;
+    let techContainer = document.querySelector('.skills__technical-container');
+    let projessionalContainer = document.querySelector('.skills__professional-container');
+    let techData = '';
+    let proData = '';
+    technical.forEach(t => {
+        techData +=     `<div class="skill">
+                            <i class="far fa-check-square skill__box"></i>
+                            <div class="skill__content">${t}</div>
+                        </div> `
+    });
+    professional.forEach(p => {
+        proData += `<div class="skill">
+                        <i class="far fa-check-square skill__box"></i>
+                        <div class="skill__content">${p}</div>
+                    </div> `
+    });
+    techContainer.innerHTML += techData;
+    projessionalContainer.innerHTML += proData;
 }
 
 
 
 // experience render
 function renderExperienceSection(data) {
-
+    data.forEach(exp => {
+        let type = exp.type,
+            title = exp.title,
+            time = exp.time,
+            text = exp.text;
+        if (type === 'education') {
+           document.querySelector('.experience__education-container').innerHTML +=
+           `<div class="card mb-30">
+                <div class="card__head">
+                    <h4>
+                    ${title}
+                    </h4>
+                </div>
+                <div class="card__body">
+                    <div class="green mt-10 mb-10">${time}</div>
+                    <div class="text-1">
+                        ${text}
+                    </div>
+                </div>
+            </div>`;
+            
+        }else if (type = 'work') {
+            // do something
+            document.querySelector('.experience__work-container').innerHTML +=
+                `<div class="card mb-30">
+                    <div class="card__head">
+                        <h4>
+                            ${title}
+                        </h4>
+                    </div>
+                    <div class="card__body">
+                        <div class="green mt-10 mb-10">${time}</div>
+                        <div class="text-1">
+                          ${text}
+                        </div>
+                    </div>
+                </div>`;
+        }
+    });
 }
 
 
